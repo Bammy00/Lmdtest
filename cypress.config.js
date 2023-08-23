a389ede72fabc4c746515ded911afc29f044cd38
Ayobami <ayobami.eleyinmi@gmail.com>
2023-08-23 12:06:29 +0100

    +Test reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,24 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
+const cucumber = require("cypress-cucumber-preprocessor").default;
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on("file:preprocessor", cucumber());
       // implement node event listeners here
-      
     },
-    baseUrl: 'https://ecommerce-playground.lambdatest.io/',
+    baseUrl: "https://ecommerce-playground.lambdatest.io/",
     specPattern: "cypress/e2e/*.{feature,js}",
-    viewportHeight:1080,
-    viewportWidth:1920
-
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportFilename: "cypressMochawesomeReport",
+      videoOnFailOnly: "true",
+      overwrite: true,
+    },
+    screenshots: {
+      enabled: true,
+    },
   },
 });
